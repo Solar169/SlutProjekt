83629314f052743b80a67e085b2ecf71c5de15e1
Herman Ahlman <herman.ahlman@elev.ga.ntig.se>
2026-03-06 11:42:02 +0100

    lektionen slut

--- SlutProjekt/Main.cs
+++ SlutProjekt/Main.cs
@@ -1,16 +1,46 @@
+using System.Numerics;
 using Raylib_cs;
 
 
 Raylib.InitWindow(800, 600, "slutprojekt");
 Raylib.SetTargetFPS(30);
 
+     Vector2 position = new Vector2(350f, 200f);
+
+     Vector2 movementX = new Vector2(10, 0);
+
+     Vector2 movemntY = new Vector2(0, 10);
+
+
 while (!Raylib.WindowShouldClose())
 {
-    Raylib.BeginDrawing();
+ Raylib.BeginDrawing();
 
-    Raylib.ClearBackground(Color.White);
+ Raylib.ClearBackground(Color.Black);
 
-    Raylib.DrawCircle(400, 300, 100, Color.Violet);
+ if (Raylib.IsKeyDown(KeyboardKey.A))
+    {
+        position -= movementX;
+    }
 
-    Raylib.EndDrawing();
+ if (Raylib.IsKeyDown(KeyboardKey.D))
+    {
+        position += movementX;
+    }
+
+ if (Raylib.IsKeyDown(KeyboardKey.W))
+    {
+        position -= movemntY;
+    }
+
+ if (Raylib.IsKeyDown(KeyboardKey.S))
+    {
+        position += movemntY;
+    }
+
+ Raylib.DrawCircleSector(position, 50, 0, -45, 100, Color.Violet);
+
+ Raylib.DrawEllipse(100, 250, 50, 100, Color.Red);
+
+ Raylib.EndDrawing();
 }
